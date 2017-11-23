5a47cc370fdc43259d3e55c3fc2c199774860483
rodrigoj42 <rodrigoj42@gmail.com>
2017-11-23 20:48:08 -0200

    syntax

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EEL840
+Código do contrato gerado para a disciplina Tópicos Especiais em Sistemas de Comunicação
--- contract.sol
+++ contract.sol
@@ -6,44 +6,47 @@ contract Mark1 {
     
     string public name;
     string public symbol;
-    uint256 public totalSupply;
+    uint256 public totalWeight;
 
-    mapping ( address => uint256) private balanceOf;
+    mapping (address => uint256) private votingWeight;
 
     // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     function Mark1 (
-        uint256 initialSupply,
+        uint256 initialWeight,
         string tokenName,
         string tokenSymbol
-    ) public {
-        totalSupply = initialSupply;
-        balanceOf[msg.sender] = totalSupply;
+    ) public 
+    {
+        totalWeight = initialWeight;
+        votingWeight[msg.sender] = totalWeight;
         name = tokenName;
         symbol = tokenSymbol;
     }
 
 
-    function _transfer(address _from, address _to , uint256 _value){
+    function _transfer(address _from, address _to, uint256 _value) internal 
+    {
 
-        require ( _to != 0x0 );
-        require ( balanceOf[_from] >= _value);
-        require ( balanceOf[_to] + value > balanceOf[_to]);
+        require (_to != 0x0);
+        require (votingWeight[_from] >= _value);
+        require (votingWeight[_to] + _value > votingWeight[_to]);
 
-        uint256previousBalances = balanceOf[_from] + balanceOf[_to];
+        uint256 previousBalances = votingWeight[_from] + votingWeight[_to];
 
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
+        votingWeight[_from] -= _value;
+        votingWeight[_to] += _value;
 
-        Transfer( _from , _to , _value );
+        Transfer(_from, _to, _value );
         
-        assert( balanceOf[_from] + balanceOf[_to] == previousBalances );
+        assert(votingWeight[_from] + votingWeight[_to] == previousBalances);
 
     }
 
-    function transfer(address _to , uint256 _value){
-        _transfer( msg.sender , _to , _value);
+    function transfer(address _to, uint256 _value) public 
+    {
+        _transfer(msg.sender, _to, _value);
     }
 
 }
