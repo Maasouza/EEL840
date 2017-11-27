98bf450db1d33788f489e07a000b3c066c8372ac
rodrigoj42 <rodrigoj42@gmail.com>
2017-11-26 22:55:16 -0200

    documentation and comments

--- contract.sol
+++ contract.sol
@@ -6,8 +6,8 @@ contract Mark4 {
     string public name;
     string public symbol;
     uint256 public totalSupply;
-    uint256 public decimals = 0;
     uint public initID;
+    uint256 decimals = 0;
 
     struct Voter {
         mapping (address => uint) delegatesWeight; // para quem você delegou votos e quantos votos foram delegados
@@ -41,6 +41,8 @@ contract Mark4 {
      * Constructor function
      *
      * Initializes contract with initial supply tokens to the creator of the contract
+     * @param tokenName Nome do token que representará o peso de um voto
+     * @param tokenSymbol Símbolo de 3 caracteres que representará o token
      */
     function Mark4 (
         string tokenName, // nome do token/voto
@@ -76,19 +78,43 @@ contract Mark4 {
         assert(proposals[initID-1].exists);
     }
 
-
-    function balanceOf(address _owner) constant public returns (uint256 balance) {
+    /**
+     * How many votes a person has
+     */
+    function balanceOf(address _owner) public constant returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function describeProposal(uint propID) public view returns (string proposalName, string description, uint minutesRemaining) {
-        return (proposals[propID].name, proposals[propID].description, (now - proposals[propID].activeUntil)/60);
+    /**
+     * Describe Proposal
+     *
+     * Gives information about a specific proposal 
+     *
+     * @param propID Proposal ID 
+     */
+    function describeProposal(uint propID) public view returns (
+        string proposalName, string description, uint minutesRemaining) {
+        return (proposals[propID].name, proposals[propID].description, (proposals[propID].activeUntil - now)/60);
     }
 
+    /**
+     * Delegated Votes
+     * 
+     * Show how many votes you delegated to a specific person
+     * 
+     * @param _to Who you delegated votes to
+     */
     function delegatedVotes(address _to) public view returns (uint votes) {
         return (voters[msg.sender].delegatesWeight[_to]);
     }
 
+    /**
+     * Proposal Result 
+     * 
+     * Shows what is the winner proposition and how many votes that proposition had
+     *
+     * @param propID Proposal ID  
+     */
     function proposalResult(uint propID) public {
         require(proposals[propID].exists);
         require(proposals[propID].activeUntil < now);
@@ -97,10 +123,24 @@ contract Mark4 {
         
     }
 
+    /**
+     * Partial Result
+     *
+     * Shows how many votes each proposition had 
+     *
+     * @param propID Proposal ID 
+     */
     function partialResult(uint propID) public view returns (uint sim, uint nao) {
         return (proposals[propID].votes["sim"], proposals[propID].votes["nao"]);
     }
 
+    /**
+     * Vote in a specific proposal 
+     * 
+     * @param proposal Proposal ID
+     * @param agree Do you agree with this proposal?
+     * @param value How many votes would you like to give? 
+     */
     function vote(uint proposal, bool agree, uint value) public {
         require(balances[msg.sender] >= value);
         require(proposals[proposal].exists);
@@ -118,26 +158,23 @@ contract Mark4 {
         assert(previousBalances == balances[msg.sender] + proposals[proposal].votes[selectedOption]);
     }
 
-    function _undoVote(address from, uint proposal, bool option, uint value) internal {
-        require(proposals[proposal].exists);
-        string storage selectedOption =  proposals[proposal].options[conversor(option)];
-        require(proposals[proposal].votes[selectedOption] >= value);
-        require(voters[from].voted[selectedOption] >= value);
-
-        uint previousBalances = balances[from] + proposals[proposal].votes[selectedOption];
-
-        proposals[proposal].votes[selectedOption] -= value;
-        voters[from].voted[selectedOption] -= value;
-        balances[from] += value;
-        string memory _selectedOption = option ? "sim" : "nao";
-        UndoVote(from, proposal, _selectedOption, value);
-        assert(previousBalances == balances[from] + proposals[proposal].votes[selectedOption]);
-    }
-
+    /**
+     * Undo a vote in a specific proposal
+     *  
+     * @param proposal Proposal ID
+     * @param option Did you agree with the proposal?
+     * @param value How many votes you want to get back
+     */
     function undoVote(uint proposal, bool option, uint value) public {
         _undoVote(msg.sender, proposal, option, value);
     }
     
+    /**
+     * Get Vote tokens.
+     *
+     * Increments your voting weight by one.
+     * 
+     */
     function authenticate() public {
         faucet(msg.sender);
     }
@@ -155,23 +192,59 @@ contract Mark4 {
         return true;
     }
 
+   /**
+    * Ask Back
+    * 
+    * Asks someone whom you delegated your votes to give them back 
+    *
+    * @param _to Who would you like to ask to give back your votes 
+    * @param _value How many votes you'd like to get back  
+    */
     function askBack(address _to, uint256 _value) public {
         _askBack(msg.sender, _to, _value);
     }
+    
+    //==============================================================================
+    // start of internal functions. do not write internal functions above this line. 
+    //==============================================================================
 
     /**
-     *  dá 1 voto a qualquer pessoa que pedir
-     *  ideia é implementar autenticação nessa função. 1 eleitor = 1 voto
+     *
      */
     function faucet(address _user) internal {
         balances[_user] += 1;
         totalSupply += 1;
     }
 
+    /**
+     *  
+     */
+    function _undoVote(address from, uint proposal, bool option, uint value) internal {
+        require(proposals[proposal].exists);
+        string storage selectedOption =  proposals[proposal].options[conversor(option)];
+        require(proposals[proposal].votes[selectedOption] >= value);
+        require(voters[from].voted[selectedOption] >= value);
+
+        uint previousBalances = balances[from] + proposals[proposal].votes[selectedOption];
+
+        proposals[proposal].votes[selectedOption] -= value;
+        voters[from].voted[selectedOption] -= value;
+        balances[from] += value;
+        string memory _selectedOption = option ? "sim" : "nao";
+        UndoVote(from, proposal, _selectedOption, value);
+        assert(previousBalances == balances[from] + proposals[proposal].votes[selectedOption]);
+    }
+
+    /**
+     *  This was not a fun function to write. 
+     */
     function conversor(bool option) internal pure returns (uint converted) {
         return (option) ? 1 : 0;
     }
 
+    /**
+     *  
+     */
     function min(uint a, uint b) internal pure returns (uint) {
         return (a < b) ? a : b;
     }
@@ -244,15 +317,10 @@ contract Mark4 {
 
     }
 
-   /**
-    * Ask Back
-    * Pede a um delegado que devolva os votos que foram emprestados a ele ou 
-    * devolve os votos enquanto a votação está ativa.
-    *
-    * @param _from Quem está pedindo os votos de volta
-    * @param _to Para quem os votos foram emprestados
-    * @param _value Quantos votos estão sendo pedidos 
-    */
+
+    /**
+     *  
+     */
     function _askBack(address _from, address _to, uint256 _value) internal {
         require(_to != 0x0);
         require(voters[_from].delegatesWeight[_to] != 0);
